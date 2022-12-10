2f0580b3f210782fd888cd1ac2582e4a998df16c
Wallaye <pavel.vashkevich03@gmail.com>
2022-12-10 23:05:48 +0300

    Added GeneratorOptions

---
+++ MPP_Lab4/TestGenerator.Core/Generator/GeneratorOptions.cs
@@ -0,0 +1,19 @@
+﻿namespace TestGenerator.Core.Generator;
+
+public class GeneratorOptions
+{
+    public int MaxThreadsToRead { get; set; }
+    public int MaxThreadsToWrite { get; set; }
+    public int MaxThreadsToGenerate { get; set; }
+    public string SourceDir { get; set; }
+    public string DestDir { get; set; }
+
+    public GeneratorOptions(int r, int w, int g, string src, string dest)
+    {
+        MaxThreadsToRead = r;
+        MaxThreadsToWrite = w;
+        MaxThreadsToGenerate = g;
+        SourceDir = src;
+        DestDir = dest;
+    }
+}
